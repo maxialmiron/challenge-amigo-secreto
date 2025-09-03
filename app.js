2bc806c137a36dc2c3397791308d88c63458f12c
Maximiliano Alfredo? Almiron <ARX0001169@prosegur.com>
2025-09-02 22:42:32 -0300

    ultimas modificaciones

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    document.getElementById("resultado").innerHTML = "";
+    let amigo = document.getElementById("amigo").value;
+    if(!amigo) {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+    agregarElemento(amigo);
+    document.getElementById("amigo").value = "";
+}
+
+function agregarElemento(amigo) {
+    amigos.push(amigo);
+    let nuevoLi = document.createElement("li");
+    nuevoLi.textContent = amigo;
+    document.getElementById("listaAmigos").appendChild(nuevoLi);
+}
+
+function sortearAmigo() {
+    if(amigos.length != 0) {
+        const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+        const amigoAleatorio = amigos[indiceAleatorio];
+        let nuevoLi = document.createElement("li");
+        nuevoLi.textContent = "El amigo secreto sorteado es: " + amigoAleatorio;
+        document.getElementById("resultado").appendChild(nuevoLi);
+        document.getElementById("listaAmigos").innerHTML = "";
+        amigos = [];
+    } else {
+        alert("No hay nombres para sortear")
+    }
+}
